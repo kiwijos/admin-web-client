b02da5992728ac410953ab19207a28e04359d395
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-11-29 11:09:17 +0100

    Display page based on whether the user is signed in or not

---
+++ src/routes/(protected)/dashboard/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+import {page} from '$app/stores';
+
+</script>
+
+{#if $page.data.user}
+    <h1>Dashboard</h1>
+    <p>Welcome back, <span class="font-bold text-primary-500">{$page.data.user.email}</span></p>
+    <p>Only authenticated users can see this page.</p>
+    <form action="/logout" method="POST">
+        <button type="submit" class="btn variant-filled">Log out</button>
+    </form>
+{:else}
+    <h1>Access denied</h1>
+    <p>You must be <a class="anchor" href="/login">logged in</a> to see this page.</p>
+{/if}
