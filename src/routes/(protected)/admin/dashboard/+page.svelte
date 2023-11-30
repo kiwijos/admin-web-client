9cecfcde79d6f8925bd9f2ffb4b787b7a88442ad
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-11-30 12:01:14 +0100

    Display features as clickable cards

---
+++ src/routes/(protected)/admin/dashboard/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    export let data: PageData;
+</script>
+
+<div class="mx-auto gap-8 p-16 flex">
+    {#each data.features as feature }
+        <a href={feature.link} class="block card card-hover">
+            <header class="card-header flex flex-col justify-end h-28 variant-soft-surface">
+                <h1 class="text-lg">{feature.heading}</h1>
+            </header>
+            <section class="p-4 space-y-4">
+                <h2 class="text-4xl font-bold">{feature.title}</h2>
+                <div>
+                    <p class="text-lg">{feature.description}</p>
+                </div>
+            </section>
+            <footer class="card-footer"></footer>
+        </a>
+    {/each}
+</div>
