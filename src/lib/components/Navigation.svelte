dce8ec4c9fbf3ed1e02f46070caab17f64755e78
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-01 10:29:28 +0100

    Add link to personal account page

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -19,27 +19,29 @@
 
 <nav class="flex flex-col space-y-8 p-4">
 	{#if $page.data.user}
-		<div class="flex flex-col items-center justify-center space-y-2">
+		<a href="/admin/account" class="flex flex-col items-center justify-center space-y-2">
 			<Avatar
 				initials={$page.data.user.email[0]}
 				border="border-4 border-surface-300-600-token hover:!border-primary-500"
 				cursor="cursor-pointer"
 			/>
 			<p class="font-bold text-primary-500">{$page.data.user.email}</p>
-		</div>
+		</a>
 		<form action="/logout" method="POST" use:enhance>
 			<button type="submit" class="w-full btn variant-ghost">Log out</button>
 		</form>
 		<ul>
 			{#each paths as { name, path }}
 				{@const active = $page.url.pathname === path ? 'page' : null}
-				{@const activeClass = $page.url.pathname === path ? 'btn w-full justify-start variant-soft-secondary ' : 'btn w-full justify-start hover:variant-soft-secondary'}
+				{@const activeClass =
+					$page.url.pathname === path
+						? 'btn w-full justify-start variant-soft-primary '
+						: 'btn w-full justify-start hover:variant-soft-primary'}
 				<li>
 					<a class={activeClass} aria-current={active} href={path} on:click={drawerClose}>{name}</a>
 				</li>
 			{/each}
 		</ul>
-		
 	{:else}
 		<a href="/login" class="btn btn-lg variant-filled-primary">Sign in</a>
 	{/if}
