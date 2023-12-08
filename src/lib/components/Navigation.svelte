795624982b5c18e7ce7ed03c20f3ab15c4b8527b
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-08 19:12:46 +0100

    Change layout and styling

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -17,19 +17,20 @@
 	}
 </script>
 
-<nav class="flex flex-col space-y-8 p-4">
+<div role="navigation" class="divide-y divide-surface-2 dark:divide-gray-600">
 	{#if $page.data.user}
-		<a href="/admin/account" class="flex flex-col items-center justify-center space-y-2">
-			<Avatar
-				initials={$page.data.user.email[0]}
-				border="border-4 border-surface-300-600-token hover:!border-primary-500"
-				cursor="cursor-pointer"
-			/>
-			<p class="font-bold text-primary-500">{$page.data.user.email}</p>
+		<a
+			href="/me/account"
+			class="flex px-4 py-3 hover:bg-gray-200 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+		>
+			<div><Avatar width="w-10" initials={$page.data.user.email[0]} /></div>
+			<div class="overflow-auto">
+				<span class="ms-3 block text-sm text-gray-900 truncate dark:text-white">Name</span>
+				<span class="ms-3 block text-sm text-gray-500 truncate dark:text-gray-400"
+					>{$page.data.user.email}</span
+				>
+			</div>
 		</a>
-		<form action="/logout" method="POST" use:enhance>
-			<button type="submit" class="w-full btn variant-ghost">Log out</button>
-		</form>
 		<ul>
 			{#each paths as { name, path }}
 				{@const active = $page.url.pathname === path ? 'page' : null}
@@ -38,11 +39,23 @@
 						? 'btn w-full justify-start variant-soft-primary '
 						: 'btn w-full justify-start hover:variant-soft-primary'}
 				<li>
-					<a class={activeClass} aria-current={active} href={path} on:click={drawerClose}>{name}</a>
+					<a
+						class="flex px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+						aria-current={active}
+						href={path}
+						on:click={drawerClose}>{name}</a
+					>
 				</li>
 			{/each}
 		</ul>
+		<form action="/logout" method="POST" use:enhance>
+			<button
+				type="submit"
+				class="block w-full px-4 py-2 text-sm text-left text-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+				>Logga ut</button
+			>
+		</form>
 	{:else}
-		<a href="/login" class="btn btn-lg variant-filled-primary">Sign in</a>
+		<a href="/login" class="btn btn-lg variant-filled-primary">Logga in</a>
 	{/if}
-</nav>
+</div>
