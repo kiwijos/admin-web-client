43ab640bd3cb89bcbd8071ae2fc16bed40f3209f
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-10 21:35:14 +0100

    Adjust style slightly and add icons and subpaths to existing paths

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -5,10 +5,34 @@
 	import { Avatar } from '@skeletonlabs/skeleton';
 	import { brand } from '$lib/config';
 
+	import Fa from 'svelte-fa';
+	import {
+		faUsersCog,
+		faMapLocation,
+		faChartPie,
+		faSignOutAlt
+	} from '@fortawesome/free-solid-svg-icons';
+
 	const paths = [
-		{ name: 'Översikt', path: '/admin/dashboard' },
-		{ name: 'Karttjänster', path: '/admin/map' },
-		{ name: 'Användare', path: '/admin/users', icon: '' }
+		{ name: 'Översikt', path: '/admin/dashboard', icon: faChartPie, subpaths: [] },
+		{
+			name: 'Kartor',
+			path: '/admin/map',
+			icon: faMapLocation,
+			subpaths: [
+				{ name: 'Zoner', path: '/admin/map/zones' },
+				{ name: 'Cyklar', path: '/admin/map/live' }
+			]
+		},
+		{
+			name: 'Användare',
+			path: '/admin/users',
+			icon: faUsersCog,
+			subpaths: [
+				{ name: 'Betalningar', path: '/admin/users/transactions' },
+				{ name: 'Resor', path: '/admin/users/trips' }
+			]
+		}
 	];
 
 	const drawerStore = getDrawerStore();
@@ -16,47 +40,75 @@
 	function drawerClose(): void {
 		drawerStore.close();
 	}
+
+	const activeClass = 'bg-surface-200 dark:bg-surface-700 dark:!text-white';
 </script>
 
 <div
 	role="navigation"
-	class="flex flex-col h-full divide-y divide-surface-2 dark:divide-surface-800"
+	class="flex flex-col h-full divide-y divide-surface-300 dark:divide-surface-700"
 >
 	{#if $page.data.user}
 		<a
-			href="/me/account"
+			href="/admin/account"
 			class="flex px-4 py-3 hover:bg-surface-200 dark:hover:bg-surface-700 dark:text-surface-200 dark:hover:text-white"
 		>
-			<div><Avatar width="w-10" initials={$page.data.user.email[0]} /></div>
+			<div>
+				<Avatar
+					width="w-10"
+					background="bg-surface-300 dark:bg-surface-500"
+					initials={$page.data.user.email[0]}
+				/>
+			</div>
 			<div class="overflow-auto">
 				<span class="ms-3 block text-sm text-surface-900 truncate dark:text-white">Namn</span>
-				<span class="ms-3 block text-sm text-surface-500 truncate dark:text-surface-400"
+				<span class="ms-3 block text-sm text-surface-400 truncate dark:text-surface-400"
 					>{$page.data.user.email}</span
 				>
 			</div>
 		</a>
 		<ul>
-			{#each paths as { name, path, icon }}
+			{#each paths as { name, path, icon, subpaths }}
 				{@const active = $page.url.pathname === path ? 'page' : null}
-				{@const activeClass =
-					$page.url.pathname === path ? 'bg-surface-200 dark:bg-surface-800' : ''}
 				<li>
-					<a
-						class="flex px-4 py-2 text-sm text-surface-700 hover:bg-surface-200 dark:hover:bg-surface-700 dark:text-surface-200 dark:hover:text-white {activeClass}"
-						aria-current={active}
-						href={path}
-						on:click={drawerClose}
-					>
-						<span class="ms-3">{name}</span>
-					</a>
+					<ul>
+						<a
+							class="flex items-center px-4 py-2 text-sm text-surface-700 hover:bg-surface-200 dark:hover:bg-surface-700 dark:text-surface-100 dark:hover:text-white {$page
+								.url.pathname === path
+								? activeClass
+								: ''}"
+							aria-current={active}
+							href={path}
+							on:click={drawerClose}
+						>
+							<Fa {icon} />
+							<span class="ms-3">{name}</span>
+						</a>
+						{#each subpaths as { name, path }}
+							{@const active = $page.url.pathname === path ? 'page' : null}
+							<li>
+								<a
+									class="flex px-4 py-2 text-sm text-surface-400 hover:bg-surface-200 dark:hover:bg-surface-700 dark:text-surface-300 dark:hover:text-white {$page
+										.url.pathname === path
+										? activeClass
+										: ''}"
+									aria-current={active}
+									href={path}
+									on:click={drawerClose}
+								>
+									<span class="ms-7">{name}</span>
+								</a>
+							</li>
+						{/each}
+					</ul>
 				</li>
 			{/each}
 		</ul>
 		<form action="/logout" method="POST" use:enhance>
 			<button
 				type="submit"
-				class="block w-full px-4 py-2 text-sm text-left text-surface-700 hover:bg-surface-200 dark:hover:bg-surface-700 dark:text-surface-200 dark:hover:text-white"
-				><span class="ms-3">Logga ut</span></button
+				class="flex items-center w-full px-4 py-2 text-sm text-left text-surface-700 hover:bg-surface-200 dark:hover:bg-surface-700 dark:text-surface-200 dark:hover:text-white"
+				><Fa icon={faSignOutAlt} /><span class="ms-3">Logga ut</span></button
 			>
 		</form>
 	{:else}
@@ -67,12 +119,9 @@
 	<!-- This is a hack to make the footer stick to the bottom -->
 
 	<div class="p-4">
-		<p class="text-sm text-left text-surface-500">Powered by</p>
-		<a
-			class="block font-extrabold text-2xl text-secondary-500"
-			href="/"
-			target="_blank"
-			rel="noopener">{brand}</a
+		<p class="text-sm text-left text-surface-400 dark:text-surface-300">Powered by</p>
+		<a class="block font-extrabold text-2xl dark:text-surface-100" href="/" target="_blank"
+			>{brand}</a
 		>
 	</div>
 </div>
