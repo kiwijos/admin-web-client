0a752854e16a716796119b1fc09c9e70cca45428
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-15 14:17:00 +0100

    Update path since the old route no longer has a Map component, also omit await keyword from expression ehich it has no effect on

--- tests/test.ts
+++ tests/test.ts
@@ -2,9 +2,9 @@ import { expect, test } from '@playwright/test';
 
 test.describe('map page test', () => {
 	test('map page has one expected map container', async ({ page }) => {
-		await page.goto('/admin/map');
+		await page.goto('/admin/map/zones');
 		const mapContainer = page.locator('.map');
-		await expect(mapContainer).toBeTruthy();
+		expect(mapContainer).toBeTruthy();
 		await expect(mapContainer).toHaveCount(1);
 	});
 });
