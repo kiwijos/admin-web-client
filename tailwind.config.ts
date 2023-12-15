5ab6870570b6a99a75f2057db05daa4f1c50a680
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-15 14:05:53 +0100

    Remove unused themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
 		forms,
 		// 4. Append the Skeleton plugin (after other plugins)
 		skeleton({
-			themes: { preset: ['hamlindigo', 'rocket', 'crimson'] }
+			themes: { preset: ['crimson'] }
 		})
 	]
 };
