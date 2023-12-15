490aa1650b552c6566286580d73ba07d0628041f
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-15 13:33:58 +0100

    Create component for displaying breadcrumbs

---
+++ src/lib/components/Breadcrumb.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	// map route names in english to their swedish counterparts to display in breadcrumbs
+	const names: { [key: string]: string } = {
+		users: 'Användare',
+		transactions: 'Betalningar',
+		trips: 'Resor'
+	};
+
+	// create breadcrumbs for all pages after /admin
+	$: myBreadcrumbs = $page.url.pathname
+		.split('/')
+		.slice(2)
+		.map((crumb, i) => {
+			return {
+				label: names[crumb] || crumb,
+				link: $page.url.pathname
+					.split('/')
+					.slice(0, i + 3)
+					.join('/')
+			};
+		});
+</script>
+
+<ol class="breadcrumb">
+	{#each myBreadcrumbs as crumb, i}
+		{#if i < myBreadcrumbs.length - 1}
+			<li class="crumb"><a class="anchor" href={crumb.link}>{crumb.label}</a></li>
+			<li class="crumb-separator" aria-hidden>/</li>
+		{:else}
+			<li class="crumb">{crumb.label}</li>
+		{/if}
+	{/each}
+</ol>
