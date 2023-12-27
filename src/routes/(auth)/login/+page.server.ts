9b285ce4cfa03f86b79e71ef876e92382f4db715
kiwijos <allq22@student.bth.se>
2023-12-27 19:56:59 +0100

    Login user via rest server

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,5 +1,6 @@
 import { fail, redirect } from '@sveltejs/kit';
 import type { Action, Actions, PageServerLoad } from './$types';
+import { PUBLIC_REST_API_URL } from '$env/static/public';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	if (locals.user) {
@@ -18,14 +19,30 @@ const login: Action = async ({ cookies, request }) => {
 		return fail(400, { invalid: true });
 	}
 
-	cookies.set('session', email, {
-		path: '/',
-		sameSite: 'strict',
-		maxAge: 60 * 60 * 24 * 30
-	});
+	try {
+		const response = await fetch(`${PUBLIC_REST_API_URL}/login/admin`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ username: email, password: password })
+		});
 
-	// redirect the user
-	throw redirect(302, '/admin/dashboard');
+		const result = await response.json();
+
+		const token = result.data.token;
+
+		cookies.set('session', token, {
+			path: '/',
+			sameSite: 'strict',
+			maxAge: 60 * 60 * 24 * 30
+		});
+
+		throw redirect(302, '/admin/dashboard');
+	} catch (error) {
+		console.error(error);
+		return fail(500, { error: true });
+	}
 };
 
 export const actions: Actions = { login };
