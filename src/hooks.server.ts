4e38ffcdf652c14224c7cdbb84eb12e52e941026
kiwijos <allq22@student.bth.se>
2023-12-27 21:49:45 +0100

    Check token before every request

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,41 @@
 import type { Handle } from '@sveltejs/kit';
+import { jwtDecode } from 'jwt-decode';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const email = event.cookies.get('session');
+	const token = event.cookies.get('session');
 
-	// Add the user's email to the request
-	if (email) event.locals.user = { email };
+	// Resolve early if there's no token to check
+	if (typeof token !== 'string' || !token) {
+		// Sometimes the user object is still in the request locals, for example if:
+		// 	- the cookie expired
+		// 	- the cookie was removed manually (e.g. via the browser dev tools)
+		// The user object is only explicitly set to null when:
+		// 	- the user logs out via /logout
+		// 	- the token check fails (e.g. invalid or expired token)
+		event.locals.user = null;
+		return await resolve(event);
+	}
+
+	try {
+		const decoded: JwtPayload = jwtDecode(token);
+
+		const exp = decoded.exp;
+		const date = new Date(0);
+		date.setUTCSeconds(exp);
+
+		if (date < new Date()) throw new Error('Token expired');
+
+		// Valid token, pass on the user object via the request locals
+		event.locals.user = {
+			id: decoded.id
+		};
+	} catch (error) {
+		console.error(error);
+
+		// Invalid token, force the user to log in again
+		event.locals.user = null;
+		event.cookies.delelte('session', { path: '/' });
+	}
 
 	return await resolve(event);
 };
