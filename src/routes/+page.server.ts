94741c9db9397320df439e59d7a64bdaa8b8c9cb
kiwijos <allq22@student.bth.se>
2023-12-28 21:20:06 +0100

    Attempt to redirect user to dashboard if the cookie seems valid

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,9 @@
 import type { PageServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
-export const load: PageServerLoad = async ({ locals }) => {
-	if (locals.user) {
-		// redirect logged in users to the dashboard
+export const load: PageServerLoad = async ({ cookies }) => {
+	if (typeof cookies.get('session') === 'string') {
+		// attempt to redirect logged in users to the dashboard
 		throw redirect(302, '/admin/dashboard');
 	}
 
