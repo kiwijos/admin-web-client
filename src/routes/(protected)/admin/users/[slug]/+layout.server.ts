74c9ebeb652f39e32582b76f8f1ffe52a39fa44c
kiwijos <allq22@student.bth.se>
2023-12-29 21:48:12 +0100

    Remove needless load file

--- src/routes/(protected)/admin/users/[slug]/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-
-// pass `locals.user` to the `page` store for use inside client-side code
-export const load: LayoutServerLoad = async ({ params }) => {
-	return {
-		id: params.slug
-	};
-};
