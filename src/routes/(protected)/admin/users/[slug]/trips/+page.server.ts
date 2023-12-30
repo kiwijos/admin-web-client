a7c4d4187133332d79cd19f8ba330a956b8ffa61
kiwijos <allq22@student.bth.se>
2023-12-30 10:48:35 +0100

    Fetch trips for user by id

---
+++ src/routes/(protected)/admin/users/[slug]/trips/+page.server.ts
@@ -0,0 +1,15 @@
+import type { PageServerLoad } from './$types';
+import { PUBLIC_REST_API_URL } from '$env/static/public';
+
+export const load: PageServerLoad = async ({ cookies, fetch, params }) => {
+	return {
+		trips: await fetch(`${PUBLIC_REST_API_URL}/admin/trips`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'x-access-token': cookies.get('session')
+			},
+			body: JSON.stringify({ user_id: params.slug })
+		}).then((r) => r.json())
+	};
+};
