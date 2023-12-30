2bf208447933f416b75c827c8c1ce905a989d85d
kiwijos <allq22@student.bth.se>
2023-12-30 20:44:06 +0100

    Replace /users route with /users/search and remove the latter

--- src/routes/(protected)/admin/users/+page.server.ts
+++ src/routes/(protected)/admin/users/+page.server.ts
@@ -1,10 +1,14 @@
 import type { PageServerLoad } from './$types';
-import { generateUsers } from '$lib/data/users';
+import { PUBLIC_REST_API_URL } from '$env/static/public';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ cookies, fetch }) => {
 	return {
-		props: {
-			users: generateUsers(100)
-		}
+		users: await fetch(`${PUBLIC_REST_API_URL}/admin/users`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				'x-access-token': cookies.get('session')
+			}
+		}).then((r) => r.json())
 	};
 };
--- src/routes/(protected)/admin/users/+page.svelte
+++ src/routes/(protected)/admin/users/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import { Paginator } from '@skeletonlabs/skeleton';
 	import type { PaginationSettings } from '@skeletonlabs/skeleton';
+	import type { User } from '$lib/types/User';
+
+	import { Paginator } from '@skeletonlabs/skeleton';
+
 	import Fa from 'svelte-fa';
 	import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
@@ -9,32 +12,32 @@
 
 	// Filter the data
 	let filter: string = '';
-	$: filteredBody = data?.props.users.filter((user) => {
+	$: filteredBody = data?.users.filter((user: User) => {
 		return user.email.toLowerCase().startsWith(filter.toLowerCase());
 	});
 
 	// Filter function
 	function filterUsers(event: Event) {
 		filter = (event.target as HTMLInputElement).value;
-		console.log(filter);
-		console.log(filteredBody);
 	}
 
 	// Slice the data to fit the pagination
+	let sourceBodySliced: User[];
 	$: sourceBodySliced = filteredBody.slice(
 		paginationSettings.page * paginationSettings.limit,
 		paginationSettings.page * paginationSettings.limit + paginationSettings.limit
 	);
 
-	$: size = data?.props.users.length;
+	let size: number;
+	$: size = data?.users.length;
 
 	// Same goes for the pagination settings
 	let paginationSettings: PaginationSettings;
 	$: paginationSettings = {
 		page: 0,
-		limit: 10,
+		limit: 50,
 		size: size,
-		amounts: [5, 10, 50]
+		amounts: [5, 10, 50, 100, 500, 1000]
 	};
 </script>
 
@@ -61,44 +64,55 @@
 		controlVariant="text-sm bg-white dark:bg-surface-600 border-transparent"
 	/>
 </div>
-<div class="relative overflow-x-auto">
-	<table class="w-full text-md text-left text-surface-400 dark:text-surface-300">
-		<thead
-			class="text-xs text-surface-700 uppercase bg-gray-50 dark:text-surface-300 dark:bg-surface-600"
-		>
-			<tr>
-				<th class="px-6 py-3 table-cell-fit">ID</th>
-				<th class="px-6 py-3">Mail</th>
-				<th class="px-6 py-3">Kortnummer</th>
-				<th class="px-6 py-3">Kort</th>
-				<th class="px-6 py-3 table-cell-fit">Saldo</th>
-				<th class="px-6 py-3">
-					<span class="sr-only">Ändra</span>
-				</th>
-			</tr>
-		</thead>
-		<tbody class="overflow-y-scroll text-sm">
-			{#each sourceBodySliced as row, i}
-				{@const negative = row.balance < 0}
-				<tr class="odd:bg-white odd:dark:bg-surface-900 even:bg-gray-50 even:dark:bg-surface-800">
-					<td class="px-6 py-4 table-cell-fit">{row.id}</td>
-					<td class="px-6 py-4">{row.email}</td>
-					<td class="px-6 py-4">{row.card_nr}</td>
-					<td class="px-6 py-4">{row.card_type}</td>
-					<td class="px-6 py-4 table-cell-fit text-right"
-						><span class="badge {negative ? ' variant-soft-error ' : 'variant-soft-success'}"
-							>{row.balance} kr</span
-						>
-					</td>
-					<td class="px-6 py-4">
-						<a
-							href="/admin/users/{row.id}/edit"
-							class="font-medium text-secondary-600 dark:text-secondary-500 hover:underline"
-							>Ändra</a
-						>
-					</td></tr
-				>
-			{/each}
-		</tbody>
-	</table>
-</div>
+{#await data.users}
+	<div class="w-full h-full flex items-center justify-center placeholder animate-pulse">
+		Hämtar användare...
+	</div>
+{:then}
+	<div class="relative overflow-x-auto">
+		<table class="w-full text-md text-left text-surface-400 dark:text-surface-300">
+			<thead
+				class="text-xs text-surface-700 uppercase bg-gray-50 dark:text-surface-300 dark:bg-surface-600"
+			>
+				<tr>
+					<th class="px-6 py-3 table-cell-fit"><span class="sr-only">ID</span></th>
+					<th class="px-6 py-3">Mail</th>
+					<th class="px-6 py-3 table-cell-fit">Saldo</th>
+					<th class="px-6 py-3">
+						<span class="sr-only">Profil, Resor, Betalningar</span>
+					</th>
+				</tr>
+			</thead>
+			<tbody class="overflow-y-scroll text-sm" data-sveltekit-preload-data="false">
+				{#each sourceBodySliced as user, i}
+					{@const negative = user.balance < 0}
+					<tr class="odd:bg-white odd:dark:bg-surface-900 even:bg-gray-50 even:dark:bg-surface-800">
+						<td class="px-6 py-4 table-cell-fit">{user.id}</td>
+						<td class="px-6 py-4">{user.email}</td>
+						<td class="px-6 py-4 table-cell-fit text-right"
+							><span class="badge {negative ? ' variant-soft-error ' : 'variant-soft-success'}"
+								>{user.balance} kr</span
+							>
+						</td>
+						<td class="px-6 py-4 text-right">
+							<a
+								href="/admin/users/{user.id}"
+								class="font-medium text-secondary-500 hover:underline">Profil</a
+							>,
+							<a
+								href="/admin/users/{user.id}/trips"
+								class="font-medium text-secondary-500 hover:underline">Resor</a
+							>,
+							<a
+								href="/admin/users/{user.id}/transactions"
+								class="font-medium text-secondary-500 hover:underline">Betalningar</a
+							>
+						</td></tr
+					>
+				{/each}
+			</tbody>
+		</table>
+	</div>
+{:catch error}
+	<p>Fel vid hämtning av användare: {error.message}</p>
+{/await}
