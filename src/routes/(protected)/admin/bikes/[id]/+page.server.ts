bbdc40df17009783b08d119829b025a5aec19849
kiwijos <allq22@student.bth.se>
2024-01-01 00:08:59 +0100

    Fetch one bike by id

---
+++ src/routes/(protected)/admin/bikes/[id]/+page.server.ts
@@ -0,0 +1,10 @@
+import type { PageServerLoad } from './$types';
+import { PUBLIC_REST_API_URL } from '$env/static/public';
+
+export const load: PageServerLoad = async ({ params }) => {
+	return {
+		bike: await fetch(`${PUBLIC_REST_API_URL}/bikes/${params.id}`, {
+			method: 'GET'
+		}).then((r) => r.json())
+	};
+};
