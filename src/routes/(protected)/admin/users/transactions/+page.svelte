fdb155741ecb04e88a5ab3c9d99e006ee5dd52c5
kiwijos <allq22@student.bth.se>
2024-01-01 14:10:13 +0100

    Add filter reset button, remove action column, with minor style changes

--- src/routes/(protected)/admin/users/transactions/+page.svelte
+++ src/routes/(protected)/admin/users/transactions/+page.svelte
@@ -4,11 +4,21 @@
 	import type { Transaction } from '$lib/types/Transaction';
 
 	import { Paginator } from '@skeletonlabs/skeleton';
+	import { formatDateReadable } from '$lib/services/dateFormatter';
 
 	export let data: PageData;
 
 	let fromDate: Date | undefined;
 	let toDate: Date | undefined;
+
+	let resetButtonActive: boolean;
+	$: resetButtonActive = fromDate ? true : toDate ? true : false;
+
+	function resetFilters() {
+		fromDate = undefined;
+		toDate = undefined;
+		paginationSettings.page = 0;
+	}
 
 	function inputFromDate(event: Event) {
 		fromDate = new Date((event.target as HTMLInputElement).value);
@@ -60,7 +70,7 @@
 	{#if transactions.length === 0}
 		<div class="w-full h-full flex items-center justify-center">Inga betalningar hittades.</div>
 	{:else}
-		<div class="bg-surface-50 dark:bg-transparent flex flex-col lg:flex-row gap-4 p-8">
+		<div class="bg-surface-50 dark:bg-transparent flex flex-col xl:flex-row gap-4 p-8">
 			<div class="flex flex-col sm:flex-row gap-4 grow">
 				<div class="input-group grid-cols-[auto_1fr_auto] border-none">
 					<div class="input-group-shim bg-white dark:bg-surface-600 text-sm">Från</div>
@@ -79,16 +89,24 @@
 					/>
 				</div>
 			</div>
-			<Paginator
-				bind:settings={paginationSettings}
-				showFirstLastButtons={true}
-				showPreviousNextButtons={true}
-				amountText="per sida"
-				separatorText="av"
-				justify="justify-between"
-				select="hover:cursor-pointer text-sm dark:bg-surface-600 rounded-3xl border-transparent"
-				controlVariant="text-sm bg-white dark:bg-surface-600 border-transparent"
-			/>
+			<div class="flex flex-col sm:flex-row gap-4 items-center">
+				<button
+					class="btn bg-white dark:bg-surface-600 text-sm w-fit"
+					disabled={!resetButtonActive}
+					on:click={resetFilters}>Rensa filter</button
+				>
+				<Paginator
+					bind:settings={paginationSettings}
+					showFirstLastButtons={true}
+					showPreviousNextButtons={true}
+					amountText="per sida"
+					separatorText="av"
+					justify="justify-normal"
+					select="hover:cursor-pointer text-sm dark:bg-surface-600 rounded-3xl border-transparent"
+					controlVariant="text-sm bg-white dark:bg-surface-600 border-transparent"
+					class=" sm:!flex-row gap-4 !space-x-0 !space-y-0 [&>label]:w-fit"
+				/>
+			</div>
 		</div>
 		<div class="relative overflow-x-auto">
 			<table class="w-full text-md text-left text-surface-400 dark:text-surface-300">
@@ -96,36 +114,36 @@
 					class="text-xs text-surface-700 uppercase bg-gray-50 dark:text-surface-300 dark:bg-surface-600"
 				>
 					<tr>
-						<th class="px-6 py-3 table-cell-fit">ID</th>
+						<th class="px-6 py-3 table-cell-fit"><span class="sr-only">ID</span></th>
 						<th class="px-6 py-3 table-cell-fit">Användare</th>
 						<th class="px-6 py-3">Kort</th>
 						<th class="px-6 py-3">Datum</th>
-						<th class="px-6 py-3">Summa</th>
-						<th class="px-6 py-3">
-							<span class="sr-only">Action</span>
-						</th>
+						<th class="px-6 py-3 table-cell-fit">Summa</th>
 					</tr>
 				</thead>
-				<tbody class="overflow-y-scroll text-sm">
-					{#each sourceBodySliced as transaction, i}
-						<tr
-							class="odd:bg-white odd:dark:bg-surface-900 even:bg-gray-50 even:dark:bg-surface-800"
-						>
-							<td class="px-6 py-4 table-cell-fit">{transaction.id}</td>
-							<td class="px-6 py-4 table-cell-fit"
-								><a
-									href="/admin/users/{transaction.user_id}"
-									class="font-medium text-secondary-500 hover:underline">{transaction.user_id}</a
-								></td
-							>
-							<td class="px-6 py-4">{transaction.ref}</td>
-							<td class="px-6 py-4">{transaction.date}</td>
-							<td class="px-6 py-4 table-cell-fit text-right">{transaction.amount} kr </td>
-							<td class="px-6 py-4 text-right">
-								<a href="#" class="font-medium text-secondary-500 hover:underline">Action</a>
-							</td>
+				<tbody class="overflow-y-scroll text-sm" data-sveltekit-preload-data="false">
+					{#if sourceBodySliced.length === 0}
+						<tr>
+							<td colspan="6" class="px-6 py-4 text-center">Inga betalningar hittades.</td>
 						</tr>
-					{/each}
+					{:else}
+						{#each sourceBodySliced as transaction, i}
+							<tr
+								class="odd:bg-white odd:dark:bg-surface-900 even:bg-gray-50 even:dark:bg-surface-800"
+							>
+								<td class="px-6 py-4 table-cell-fit">{transaction.id} </td>
+								<td class="px-6 py-4 table-cell-fit"
+									><a
+										href="/admin/users/{transaction.user_id}"
+										class="font-medium text-secondary-500 hover:underline">{transaction.user_id}</a
+									></td
+								>
+								<td class="px-6 py-4">{transaction.ref}</td>
+								<td class="px-6 py-4 whitespace-nowrap">{formatDateReadable(transaction.date)}</td>
+								<td class="px-6 py-4 table-cell-fit text-right">{transaction.amount} kr </td>
+							</tr>
+						{/each}
+					{/if}
 				</tbody>
 			</table>
 		</div>
