1efda441e64d892e432b2d03f878bbc826eeeae2
kiwijos <allq22@student.bth.se>
2024-01-01 14:07:52 +0100

    Add pagination and remove less relevant fields

--- src/routes/(protected)/admin/bikes/+page.svelte
+++ src/routes/(protected)/admin/bikes/+page.svelte
@@ -1,8 +1,31 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import ChargeMeter from '$lib/components/ChargeMeter.svelte';
+	import type { PaginationSettings } from '@skeletonlabs/skeleton';
+
+	import { Paginator } from '@skeletonlabs/skeleton';
+	import { statusCodes } from '$lib/help/statusCodes';
 
 	export let data: PageData;
+
+	// Slice the data to fit the pagination
+	// let sourceBodySliced: Trip[];
+	$: sourceBodySliced = data?.bikes.slice(
+		paginationSettings.page * paginationSettings.limit,
+		paginationSettings.page * paginationSettings.limit + paginationSettings.limit
+	);
+
+	let size: number;
+	$: size = data?.bikes.length;
+
+	// Same goes for the pagination settings
+	let paginationSettings: PaginationSettings;
+	$: paginationSettings = {
+		page: 0,
+		limit: 50,
+		size: size,
+		amounts: [5, 10, 50, 100, 500, 1000]
+	};
 </script>
 
 {#await data.bikes}
@@ -11,8 +34,21 @@
 	</div>
 {:then bikes}
 	{#if bikes.length === 0}
-		<p class="text-xs text-surface-400">Inga cyklar hittades</p>
+		<p class="text-xs text-surface-400">Inga cyklar hittades.</p>
 	{:else}
+		<div class="bg-surface-50 dark:bg-transparent p-8">
+			<Paginator
+				bind:settings={paginationSettings}
+				showFirstLastButtons={true}
+				showPreviousNextButtons={true}
+				amountText="per sida"
+				separatorText="av"
+				justify="justify-between"
+				select="hover:cursor-pointer text-sm dark:bg-surface-600 rounded-3xl border-transparent"
+				controlVariant="text-sm bg-white dark:bg-surface-600 border-transparent"
+				class="sm:!flex-row gap-4 !space-x-0 !space-y-0 [&>label]:w-fit"
+			/>
+		</div>
 		<div class="relative overflow-x-auto">
 			<table class="w-full text-md text-left text-surface-400 dark:text-surface-300">
 				<thead
@@ -21,30 +57,30 @@
 					<tr>
 						<th class="px-6 py-3"><span class="sr-only">ID</span></th>
 						<th class="px-6 py-3">Stad</th>
-						<th class="px-6 py-3">Hyrstatus</th>
+						<th class="px-6 py-3">Status</th>
 						<th class="px-6 py-3">Batteri</th>
-						<th class="px-6 py-3 table-cell-fit">Status</th>
-						<th class="px-6 py-3">
-							<span class="sr-only">Aktivera/Stoppa</span>
-						</th>
 					</tr>
 				</thead>
 				<tbody class="overflow-y-scroll text-sm" data-sveltekit-preload-data="false">
-					{#each bikes as bike, i}
+					{#each sourceBodySliced as bike, i}
 						<tr
 							class="odd:bg-white odd:dark:bg-surface-900 even:bg-gray-50 even:dark:bg-surface-800"
 						>
-							<td class="px-6 py-4 table-cell-fit">{bike.id}</td>
+							<td class="px-6 py-4 table-cell-fit"
+								><a
+									href="/admin/bikes/{bike.id}"
+									class="font-medium text-secondary-500 hover:underline whitespace-nowrap"
+									>Cykel {bike.id}</a
+								></td
+							>
 							<td class="px-6 py-4">{bike.city_id} </td>
-							<td class="px-6 py-4">{bike.status_descr} </td>
+							<td class="px-6 py-4">{statusCodes[bike.status_id]} </td>
 							<td class="px-6 py-4 grid gap-2 grid-cols-2 items-center"
 								><ChargeMeter fraction={bike.charge_perc} /><span
 									>{Math.round(bike.charge_perc * 100)}%</span
 								>
-							</td>
-							<td class="px-6 py-4">{bike.active ? 'Aktiv' : 'Inaktiv'} </td>
-							<td class="px-6 py-4 table-cell-fit"> Stop </td>
-						</tr>
+							</td></tr
+						>
 					{/each}
 				</tbody>
 			</table>
