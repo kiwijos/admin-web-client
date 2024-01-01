1a8cf18ecffd2bb24b647822d3d64c356b4c8d6a
kiwijos <allq22@student.bth.se>
2024-01-01 13:59:14 +0100

    Include breadcrumbs

---
+++ src/routes/(protected)/admin/bikes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Breadcrumb from '$lib/components/Breadcrumb.svelte';
+</script>
+
+<div class="py-2 px-4 bg-white dark:bg-surface-700 w-full text-sm">
+	<Breadcrumb />
+</div>
+<slot />
