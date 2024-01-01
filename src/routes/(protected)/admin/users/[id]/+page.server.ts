3626b39afc8a107498de2bfd2e1d41f2d829b3ff
kiwijos <allq22@student.bth.se>
2024-01-01 14:01:16 +0100

    Fetch transactions for user by id

--- src/routes/(protected)/admin/users/[id]/+page.server.ts
+++ src/routes/(protected)/admin/users/[id]/+page.server.ts
@@ -3,13 +3,21 @@ import { PUBLIC_REST_API_URL } from '$env/static/public';
 
 export const load: PageServerLoad = async ({ cookies, fetch, params }) => {
 	return {
-		users: await fetch(`${PUBLIC_REST_API_URL}/admin/users/${params.slug}`, {
+		user: await fetch(`${PUBLIC_REST_API_URL}/admin/users/${params.slug}`, {
 			method: 'GET',
 			headers: {
 				'x-access-token': cookies.get('session')
 			}
 		}).then((r) => r.json()),
 		trips: await fetch(`${PUBLIC_REST_API_URL}/admin/trips/limit/5/offset/0`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'x-access-token': cookies.get('session')
+			},
+			body: JSON.stringify({ user_id: params.slug })
+		}).then((r) => r.json()),
+		transactions: await fetch(`${PUBLIC_REST_API_URL}/admin/transactions/limit/5/offset/0`, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
