e5440789e690b21d760b25b70c55d2ec85ee7eec
kiwijos <allq22@student.bth.se>
2024-01-05 12:36:50 +0100

    Fetch initial bike positions

---
+++ src/routes/(protected)/admin/map/test/animate/+page.server.ts
@@ -0,0 +1,10 @@
+import type { PageServerLoad } from './$types';
+import { PUBLIC_REST_API_URL } from '$env/static/public';
+
+export const load: PageServerLoad = async () => {
+	return {
+		bikes: await fetch(`${PUBLIC_REST_API_URL}/bikes`, {
+			method: 'GET'
+		}).then((r) => r.json())
+	};
+};
