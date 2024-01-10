edc66bd6988c898d8e3f52c3bbb7a2a8e6b56d52
p0ntan <pontus.akerberg@hotmail.com>
2024-01-10 17:10:07 +0100

    Add handleFetch hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,9 @@
 import type { Handle } from '@sveltejs/kit';
+import type { HandleFetch } from '@sveltejs/kit';
 import { redirect } from '@sveltejs/kit';
 import { jwtDecode } from 'jwt-decode';
 import type { CustomJwtPayload } from './lib/types/CustomJwtPayload';
+import { PRIVATE_REST_API_KEY } from '$env/static/private';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	// ONLY RUN this hook for admin pages (e.g. /admin/dashboard, /admin/users)
@@ -12,6 +14,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 	const token = event.cookies.get('access_token');
 
+
 	// Redirect early to login page if the user is not logged in
 	if (typeof token !== 'string' || !token) {
 		// Sometimes the user object is still in the request locals, for example if:
@@ -21,7 +24,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 		// 	- the user logs out via /logout
 		// 	- the token check fails (e.g. invalid or expired token)
 		event.locals.signed_in_as = null; // <-- make sure the user object `signed_in_as` is null in any case
-		throw redirect(302, '/login');
+		throw redirect(302, '/');
 	}
 
 	try {
@@ -47,4 +50,10 @@ export const handle: Handle = async ({ event, resolve }) => {
 	}
 
 	return await resolve(event); // <-- all good, allow the request to proceed
+};
+
+export const handleFetch: HandleFetch = async ({ request, fetch }) => {
+	request.headers.set('x-api-key', PRIVATE_REST_API_KEY);
+
+	return fetch(request);
 };
