2b09165526f22f7d232d41e3b15726797b280fb8
kiwijos <allq22@student.bth.se>
2024-01-13 11:16:18 +0100

    Add fields

--- src/lib/types/ZonePolygonFeature.ts
+++ src/lib/types/ZonePolygonFeature.ts
@@ -1,8 +1,13 @@
+import type { Bike } from './Bike';
+
 export interface ZonePolygonFeature {
 	type: 'Feature';
 	geometry: { type: string; coordinates: [number, number][][] };
 	properties: {
 		id: number;
+		zone_id: number;
+		bike_count?: number;
+		bikes?: Bike[];
 		descr: string;
 		city_id: string;
 	};
