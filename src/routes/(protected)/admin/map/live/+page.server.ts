e9dd56ab34683e7bdf6cefce29a9bb656a1bbc27
kiwijos <allq22@student.bth.se>
2024-01-13 11:18:25 +0100

    Fetch zones and cities too

--- src/routes/(protected)/admin/map/live/+page.server.ts
+++ src/routes/(protected)/admin/map/live/+page.server.ts
@@ -3,6 +3,8 @@ import { PUBLIC_REST_API_URL } from '$env/static/public';
 
 export const load: PageServerLoad = async ({ fetch }) => {
 	return {
-		bikes: await fetch(`${PUBLIC_REST_API_URL}/bikes`).then((r) => r.json())
+		bikes: await fetch(`${PUBLIC_REST_API_URL}/bikes`).then((r) => r.json()),
+		cities: await fetch(`${PUBLIC_REST_API_URL}/cities`).then((r) => r.json()),
+		zones: await fetch(`${PUBLIC_REST_API_URL}/zones`).then((r) => r.json())
 	};
 };
