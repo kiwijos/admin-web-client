f0c803166fb763d92da888a52da49f2c7ca18da2
kiwijos <allq22@student.bth.se>
2024-01-13 11:16:02 +0100

    Rename field to be consistent with zone type

--- src/lib/types/ZonePolygonFeature.ts
+++ src/lib/types/ZonePolygonFeature.ts
@@ -2,7 +2,8 @@ export interface ZonePolygonFeature {
 	type: 'Feature';
 	geometry: { type: string; coordinates: [number, number][][] };
 	properties: {
-		zone_type: string;
+		id: number;
+		descr: string;
 		city_id: string;
 	};
 }
