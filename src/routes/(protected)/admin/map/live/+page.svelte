506b75208af478f08d4416e008d9e2baf7ed35bf
kiwijos <allq22@student.bth.se>
2024-01-13 13:26:01 +0100

    Make bike points bigger

--- src/routes/(protected)/admin/map/live/+page.svelte
+++ src/routes/(protected)/admin/map/live/+page.svelte
@@ -145,7 +145,7 @@
 				filter: ['!', ['has', 'point_count']],
 				paint: {
 					'circle-color': '#11b4da',
-					'circle-radius': 4,
+					'circle-radius': 8,
 					'circle-stroke-width': 1,
 					'circle-stroke-color': '#fff'
 				}
