f4bf34422622dd0b5b8ebf20b99130de2383eab4
kiwijos <allq22@student.bth.se>
2024-01-14 23:45:57 +0100

    Remove unused file

--- src/routes/(protected)/admin/map/test/animate/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { PageServerLoad } from './$types';
-import { PUBLIC_REST_API_URL } from '$env/static/public';
-
-export const load: PageServerLoad = async ({ fetch }) => {
-	return {
-		bikes: await fetch(`${PUBLIC_REST_API_URL}/bikes`).then((r) => r.json())
-	};
-};
