e1e8687121783a651bacb17395243f408727496c
kiwijos <allq22@student.bth.se>
2024-01-17 18:55:52 +0100

    Register modal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,13 @@
 	import { autoModeWatcher } from '@skeletonlabs/skeleton';
 	import { initializeStores } from '@skeletonlabs/skeleton';
 
-	import { LightSwitch } from '@skeletonlabs/skeleton';
+	import { LightSwitch, Modal } from '@skeletonlabs/skeleton';
+	import type { ModalComponent } from '@skeletonlabs/skeleton';
+	import FileInvoiceConfirmModal from '$lib/components/FileInvoiceConfirmModal.svelte';
+
+	const modalRegistry: Record<string, ModalComponent> = {
+		modalFileInvoiceConfirm: { ref: FileInvoiceConfirmModal }
+	};
 
 	initializeStores();
 </script>
@@ -11,6 +17,7 @@
 <svelte:head>
 	{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}
 </svelte:head>
+<Modal components={modalRegistry} buttonTextCancel="Avbryt" />
 <slot />
 <div class="fixed bottom-0 right-0 z-50 p-4">
 	<LightSwitch />
