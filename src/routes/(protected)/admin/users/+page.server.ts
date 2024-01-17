02e31d98bb0b7df3801b6fb9cf0930e47c608e14
kiwijos <allq22@student.bth.se>
2024-01-17 23:20:20 +0100

    Create action for updating a user's status

--- src/routes/(protected)/admin/users/+page.server.ts
+++ src/routes/(protected)/admin/users/+page.server.ts
@@ -44,4 +44,55 @@ const fileInvoice: Action = async ({ fetch }) => {
 	return { success: true, invoiceData: result };
 };
 
-export const actions: Actions = { fileInvoice };
+const updateStatus: Action = async ({ request, fetch }) => {
+	const data = await request.formData();
+
+	const activeString = data.get('active');
+	const userId = data.get('userId');
+
+	if (typeof activeString !== 'string' || !activeString) {
+		return fail(400, { active, invalid: true });
+	}
+
+	if (typeof userId !== 'string' || !userId) {
+		return fail(400, { userId, invalid: true });
+	}
+
+	const active = activeString === 'true';
+
+	let result;
+
+	try {
+		const response = await fetch(`${PUBLIC_REST_API_URL}/admin/users/${userId}/status`, {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ active })
+		});
+
+		result = await response.json();
+
+		if (!response.ok) {
+			console.error(result.errors.message);
+
+			return fail(response.status, {
+				error: true,
+				message: 'Kunde inte uppdatera status på användaren på grund av ett serverfel.'
+			});
+		}
+	} catch (error) {
+		let message;
+		if (error instanceof Error) message = error.message;
+		else message = String(error);
+
+		console.error(message);
+
+		return fail(500, {
+			error: true,
+			message: 'Kunde inte uppdatera status på användaren på grund av ett serverfel.'
+		});
+	}
+
+	return { success: true, user: result };
+};
+
+export const actions: Actions = { fileInvoice, updateStatus };
