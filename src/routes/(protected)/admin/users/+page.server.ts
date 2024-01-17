ecc2b4f45315a6c62e7486b119833c585d8c86ba
kiwijos <allq22@student.bth.se>
2024-01-17 18:54:03 +0100

    Use depends to be able to target-invalidate load function

--- src/routes/(protected)/admin/users/+page.server.ts
+++ src/routes/(protected)/admin/users/+page.server.ts
@@ -1,7 +1,8 @@
 import type { PageServerLoad } from './$types';
 import { PUBLIC_REST_API_URL } from '$env/static/public';
 
-export const load: PageServerLoad = async ({ fetch }) => {
+export const load: PageServerLoad = async ({ fetch, depends }) => {
+	depends('server:fetchUsers');
 	return {
 		users: await fetch(`${PUBLIC_REST_API_URL}/admin/users`).then((r) => r.json())
 	};
